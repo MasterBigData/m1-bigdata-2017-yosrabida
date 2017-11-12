3acc6ef0d9091bff29e20874a658eaf0e39a2ba5
yosrabida <32879713+yosrabida@users.noreply.github.com>
2017-11-12 11:52:49 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,32 +2,39 @@ package modele;
 
 import java.awt.Dimension;
 
+
 import javax.swing.JFrame;
 
 import vue.MyDisplay;
+
 
 public class Test {
 	public static void main(String []args){
 		
 
-		JFrame frame=new JFrame("Java Avancé - Graphic Display");
+		JFrame frame=new JFrame("Java AvancÃ© - Graphic Display");
 		frame.setSize(new Dimension(500,500));
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		
 		Point P1= new Point(100,50);
-		Cercle C1= new Cercle(P1,200);
 		Point P2= new Point(300,300);
-		Cercle C2= new Cercle(P2,100);
-		
-		World w = new World();
-		w.shapes.add(C1);
-		w.shapes.add(C2);
+		Cercle C1= new Cercle(P1,200);
+		Cercle C2= new Cercle(P1,400);
 
+		Ring R1= new Ring(P1,300,200);
+		Ring R2=new Ring(P2,200,170);
+		World w = new World();
+        w.shapes.add(C1);
+        w.shapes.add(C2);
+        w.shapes.add(R1);
+        w.shapes.add(R2);
 		MyDisplay d=new MyDisplay(w);
 		
 		w.addObserver(d);
 		frame.add(d);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
 
 }
