8ad33f943971a942f011f93eef022f99bdcf8787
yosrabida <32879713+yosrabida@users.noreply.github.com>
2017-11-12 16:07:56 +0100

    Update DrawableRing.java

--- DrawableRing.java
+++ DrawableRing.java
@@ -3,17 +3,19 @@ import java.awt.Graphics;
 
 import modele.Ring;
 import modele.Cercle;
+
 public class DrawableRing extends DrawableCercle {
 	public DrawableRing(Cercle s) {
 		super(s);
 	}
 
 
-
 	public void draw(Graphics G) {
+		super.draw(G);
 
-	G.drawOval( (S.getCenter().getX()),S.getCenter().getY(),((Ring) S).getRmax(),((Ring) S).getRmax());
 	G.drawOval( (S.getCenter().getX()),S.getCenter().getY(),((Ring) S).getRmin(),((Ring) S).getRmin());
 
 	}
 	}
+
+
