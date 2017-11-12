3c48d3bb12f5b8f71c6efab180f7fa70abdf380f
yosrabida <32879713+yosrabida@users.noreply.github.com>
2017-11-12 16:07:17 +0100

    Update Ring.java

--- Ring.java
+++ Ring.java
@@ -1,18 +1,15 @@
+
 package modele;
 
 import vue.DrawableRing;
-
-
 public class Ring extends Cercle{
 public int Rmin;
-public int Rmax;
 
-public Ring(Point p , int Rmax, int Rmin)
+public Ring(Point p, int Rmax ,int Rmin)
 {
 	
 super (p,Rmax);
 this.Rmin=Rmin;
-this.Rmax=Rmax;
 ds=new DrawableRing(this);
 
 }
@@ -23,20 +20,9 @@ public Ring() {
 public Point getCenter() {
 	return center;
 }
-public int getRmax() {
-	return Rmax;
-}
+
 public int getRmin() {
 	return Rmin;
-}
-public String ToString (Ring A){
-	return (A.getCenter()+","+A.getRmax()+","+A.getRmin());
-}
-public boolean contains(Point p) {
-	if (Math.sqrt(Math.pow(p.getX()-this.center.getX(),2)+Math.pow(p.getY()-this.center.getY(), 2))==this.Rmax) {
-		return true;
-	}
-	return false;
 }
 }
 
