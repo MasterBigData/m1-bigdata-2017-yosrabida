a6350bbba4070fac2d73a068d8176d5c74d9c818
yosrabida <32879713+yosrabida@users.noreply.github.com>
2017-11-14 21:10:26 +0100

    Update DrawableCercle.java

--- DrawableCercle.java
+++ DrawableCercle.java
@@ -5,13 +5,17 @@ import modele.Cercle;
 import modele.Shape;
 public class DrawableCercle extends DrawableShape {
 public DrawableCercle(Shape s) {
-	super( s);
+	super(s);
 }
+
+
+
+
 
 
 
 public void draw(Graphics G) {
 
-	G.drawOval( (S.getCenter().getX(),S.getCenter().getY(),((Cercle) S).getRayon(),((Cercle) S).getRayon());
+G.drawOval( (S.getCenter().getX()-((Cercle) S).getRayon()),S.getCenter().getY()-((Cercle) S).getRayon(),((Cercle) S).getRayon()*2,((Cercle) S).getRayon()*2);
 }
 }
